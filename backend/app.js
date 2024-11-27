213a9f57492218c60ddd80684e5adbdbf0663b07
lianecc <liane.coupat@gmail.com>
2024-11-27 11:45:03 +0100

    moving routes to new folder

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,14 @@
-
 // config principale de l'app, importation routes et middlewares 
 const express = require('express');
 const mongoose = require("mongoose");
-const Book = require("./models/book");
-
-const app = express();
+const booksRoutes = require("./routes/books"); 
 
 // Connexion à MongoDB
-  mongoose.connect("mongodb+srv://Paupiette13:13BluePaupiette@clusterlcc.7hndt.mongodb.net/mydatabase?retryWrites=true&w=majority")
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose.connect("mongodb+srv://Paupiette13:13BluePaupiette@clusterlcc.7hndt.mongodb.net/mydatabase?retryWrites=true&w=majority")
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();    
 
 // Middleware CORS
 app.use((req, res, next) => {
@@ -22,46 +21,7 @@ app.use((req, res, next) => {
 // Middleware JSON
 app.use(express.json());
 
-// route POST
-app.post("/api/books", (req, res, next) => {
-  delete req.body._id;
-  const book = new Book({
-    ...req.body
-  });
-  book.save()
-    .then(() => res.status(201).json({ message: "Livre enregistré !"}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-//route modification d'un livre 
-app.put("/api/books/:id", (req, res, next) => {
-  Book.updateOne({ _id: req.params.id }, { ...req.body, _id:req.params.id})
-  .then(() => res.status(200).json({message: "Livre modifié !"}))
-  .catch(error => res.status(400).json({error}));
-});
-
-// route suppression d'un livre 
-app.delete('/api/books/:id', (req, res, next) => {
-  Book.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Livre supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-
-// route get pour 1 seul livre
-app.get("/api/books/:id", (req, res, next) => {
-  Book.findOne({ _id: req.params.id })
-  .then(book => res.status(200).json(book))
-  .catch(error => res.status(404).json({ error }));
-});
-
-// route GET à la page d'accueil
-app.get("/api/books", (req, res,next) => {
-  Book.find()
-  .then(books =>res.status(200).json(books))
-  .catch(error => res.status (400).json ({ error }))
-});
-
-
+// Définir les routes
+app.use("/api/books", booksRoutes); 
 
 module.exports = app;
