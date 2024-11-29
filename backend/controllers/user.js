763cfc01678203b43037d6cedf5afd3d86875adf
lianecc <liane.coupat@gmail.com>
2024-11-29 23:11:57 +0100

    adding 2sec delay on user login - security measure

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -10,7 +10,7 @@ exports.signUp = (req, res, next) => {
             const user = new User({
                 email: req.body.email,
                 password: hash, 
-            });
+            });-
             user.save()
                 .then(() => res.status(201).json({ message: "Utilisateur créé" }))
                 .catch(error => {
@@ -22,41 +22,38 @@ exports.signUp = (req, res, next) => {
         });
 };
 
+// simule delai de 2s 
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
 // verifie paire user/mdp 
 exports.login = (req, res, next) => {
-    console.log("email reçu : ", req.body.email);
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            if (!user) {
-                console.log("Utilisateur introuvable.");
-
-                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
-            }
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    if (!valid) {
-                        console.log("Mot de passe incorrect.");
-
-                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
-                    }
-                    console.log("Connexion réussie pour l'utilisateur :", user._id);
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            "RANDOM_TOKEN_SECRET",
-                            { expiresIn: "24h" }
-                        )
+    delay(2000)
+        .then(() => {
+        User.findOne({ email: req.body.email })
+            .then(user => {
+                if (!user) {
+                    return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+                }
+                bcrypt.compare(req.body.password, user.password)
+                    .then(valid => {
+                        if (!valid) {
+                            return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                        }
+                        res.status(200).json({
+                            userId: user._id,
+                            token: jwt.sign(
+                                { userId: user._id },
+                                "RANDOM_TOKEN_SECRET",
+                                { expiresIn: "24h" }
+                            )
+                        });
+                    })
+                    .catch(error => {                    
+                        res.status(500).json({ error });
                     });
-                })
-                .catch(error => {                    
-                    console.error("Erreur bcrypt :", error);
-                    res.status(500).json({ error });
-                });
-        })
-        .catch(error => {
-            console.error("Erreur lors de la recherche d'utilisateur :", error);
-            res.status(500).json({ error })
-        }); // erreur d'execution de la req ds la bdd, pas si l'utilisateur n'est pas trouvé ds la bdd 
- }; 
+            })
+            .catch(error => { res.status(500).json({ error }) }); // erreur d'execution de la req ds la bdd, pas si l'utilisateur n'est pas trouvé ds la bdd    
+    })
+    .catch(error => { res.status(500).json({ error })});
+}; 
 
